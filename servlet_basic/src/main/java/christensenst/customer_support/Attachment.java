474241df7b2bb05abc526c5850066a7717853d82
Stephen Christensen <stephen.t.christensen@gmail.com>
2014-07-22 15:36:18 -0500

    Add TicketServlet for creating tickets and uploading attachments

---
+++ servlet_basic/src/main/java/christensenst/customer_support/Attachment.java
@@ -0,0 +1,26 @@
+package christensenst.customer_support;
+
+/**
+ *
+ */
+public class Attachment {
+    
+    private String name;
+    private byte[] contents;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContents() {
+        return contents;
+    }
+
+    public void setContents(byte[] contents) {
+        this.contents = contents;
+    }
+}
